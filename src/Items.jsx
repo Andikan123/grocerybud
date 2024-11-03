4e32433fbd7372616ae9aecf90a9e41d9f52ffce
Andikan <andikanudofia@gmail.com>
2024-11-03 23:52:42 +0200

    grocerybud

---
+++ src/Items.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SingleItem from "./SingleItem";
+
+const Items = ({ items, removeItem ,editItem}) => {
+  return (
+    <div className="items">
+      {items.map((item) => {
+        return <SingleItem key={item.id} removeItem={removeItem} item={item} editItem={editItem}/>;
+      })}
+    </div>
+  );
+};
+
+export default Items;
